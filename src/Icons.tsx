1d2c4d26dd51f21c708ea6c76d3d0fc1d2225c41
polatengin <enpolat@microsoft.com>
2022-04-21 05:45:16 -0700

    adding close icon component

--- src/Icons.tsx
+++ src/Icons.tsx
@@ -4,3 +4,11 @@ interface IconProps {
   className?: string;
   onClick?: () => void;
 }
+
+export const CloseIcon: FunctionComponent<IconProps> = (props: IconProps) => {
+  return (
+    <svg viewBox="0 0 370 370" fill="black" className={props.className} onClick={props.onClick}>
+      <polygon points="370,21 350,0 185,164 21,0 0,21 164,185 0,350 21,370 185,206 350,370 370,350 206,185 "/>
+    </svg>
+  );
+};
