260726f27b59721b4e745f6febd5b70bcbb6df81
polatengin <enpolat@microsoft.com>
2022-04-21 05:48:18 -0700

    adding menu items, if menu is showing on the page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,14 @@ export const App: FunctionComponent = () => {
     <div className="w-screen h-screen bg-gray-200 p-4">
       <div className="w-full h-full rounded-2xl shadow-2xl bg-white p-4">
         <h1 className="flex justify-center text-xl">Math exercises for 3rd graders</h1>
+        { showMenu &&
+          <div className="flex flex-col gap-4 mt-4">
+            <button className="py-4 bg-gray-200 rounded shadow">Two-Three digit mixed additions</button>
+            <button className="py-4 bg-gray-200 rounded shadow">Two digit substractions</button>
+            <button className="py-4 bg-gray-200 rounded shadow">Three digit substractions</button>
+            <button className="py-4 bg-gray-200 rounded shadow">Two-Three digit mixed substractions</button>
+          </div>
+        }
       </div>
     </div>
   );
