ab5c02c2b04c787d29320efe83ed0de3218f781a
polatengin <enpolat@microsoft.com>
2022-04-21 05:47:04 -0700

    adding title header to the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React, { FunctionComponent } from 'react';
 export const App: FunctionComponent = () => {
 
+  return (
+    <div className="w-screen h-screen bg-gray-200 p-4">
+      <div className="w-full h-full rounded-2xl shadow-2xl bg-white p-4">
+        <h1 className="flex justify-center text-xl">Math exercises for 3rd graders</h1>
+      </div>
+    </div>
   );
 };
